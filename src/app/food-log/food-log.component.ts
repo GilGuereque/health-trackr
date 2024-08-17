a16e596f92ef475a8414f71c1c9caaea4718f534
Gil Guereque <gguereque326@gmail.com>
2024-08-16 20:51:50 -0700

    Created profile, dashboard, food-log componets

---
+++ src/app/food-log/food-log.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-log',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-log.component.html',
+  styleUrl: './food-log.component.css'
+})
+export class FoodLogComponent {
+
+}
