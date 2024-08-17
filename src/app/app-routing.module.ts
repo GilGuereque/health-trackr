5c78a30b63bc24507198268f3e27413354c5dc34
Gil Guereque <gguereque326@gmail.com>
2024-08-16 21:14:55 -0700

    updated app.component.html file and created app.module.ts & app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FoodLogComponent } from './food-log/food-log.component';
+import { ProfileComponent } from './profile/profile.component';
+
+//// Define the routes for the applicationconstroutes: Routes = [
+//{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirect to Dashboard by default
+//{ path: 'dashboard', component: DashboardComponent },       // Route to DashboardComponent
+//{ path: 'food-log', component: FoodLogComponent },          // Route to FoodLogComponent
+//{ path: 'profile', component: ProfileComponent }            // Route to ProfileComponent
+//];
+
+//@NgModule({
+//  imports: [RouterModule.forRoot(routes)],  // Configure the router at the application's root levelexports: [RouterModule]                   // Export RouterModule to make it available throughout the app
+//})
+//exportclassAppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FoodLogComponent } from './food-log/food-log.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    FoodLogComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
